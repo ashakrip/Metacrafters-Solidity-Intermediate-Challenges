20d9f6efff8933beb8658d31d935d2fef800606a
Asha Kripalani <119749754+ashakrip@users.noreply.github.com>
2023-03-07 09:06:57 +0530

    Add files via upload

--- Moolah.sol
+++ Moolah.sol
@@ -1,12 +1,10 @@
-//SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
 contract ProjectMoolah is ERC20 {
-    
     constructor(uint256 initialSupply) ERC20("Moolah", "MOOL") {
         _mint(msg.sender, initialSupply);
     }
-    
 }
